70adc49887dcdc9c2821186f4dff58443e5b7bef
ashwinsriramulu7 <ashwinsriramulu7@gmail.com>
2025-11-03 21:20:31 +0530

    feat: added helper function to decode matlab strings

--- Dataset/matlab-to-csv.py
+++ Dataset/matlab-to-csv.py
@@ -11,3 +11,23 @@ def matlab_datenum_to_python_datetime(dn):
     """
     return datetime.datetime.fromordinal(int(dn))+datetime.timedelta(days=dn%1)-datetime.timedelta(days=366)
 
+def read_ds(f, group, key):
+    """
+    HELPER FUNCTION
+    Safely reads a dataset and returns none if key is missing
+    """
+    if key in group:
+        ref = group[key][0][0]
+        return np.array(f[ref]).flatten()
+    return None
+
+def read_matlab_string(f, ref):
+    """
+    HELPER FUNCTION
+    Decodes MATLAB character array stored in HDF5 
+    """"
+    arr = f[ref][:]
+    if arr.ndim == 2:
+        arr = arr.flatten()
+    return "".join(chr(x) for x in arr)
+    
