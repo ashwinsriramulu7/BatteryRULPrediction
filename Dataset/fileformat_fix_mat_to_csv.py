15177b1379cb9f458355b63cde643e7876fa0f0a
ashwinsriramulu7 <ashwinsriramulu7@gmail.com>
2025-11-07 18:28:59 +0530

    chore: small change in print string

--- Dataset/fileformat_fix_mat_to_csv.py
+++ Dataset/fileformat_fix_mat_to_csv.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy.io as sio
 import os
 
+#print path of all subfolders
 datadir = '/home/ashwin-sriramulu/Documents/work/BatteryRULPrediction/Dataset/Raw_Mat_Data'
 for folder in os.scandir(datadir):
-    print(f"Directory Found{folder.path}")
+    print(f"Directory Found: {folder.path}")
+
